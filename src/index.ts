8ef4f2b161b0a3e24ab554cdb4f6113ca5824c94
Tusharsd123 <www.tushardiwakar@gmail.com>
2021-10-24 13:36:43 +0530

    database entities creation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Posts";
+
+const main = async () => {
+const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: 'YOUSAY',
+    type: 'postgresql',
+    debug: !__prod__,
+});
+}
+
+main();
+
